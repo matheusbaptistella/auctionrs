92c74ca8e8cb0f1bfeb26054ebc2e9e2e557640e
Matheus Baptistella <mbapts@gmail.com>
2025-07-15 18:56:35 -0300

    implement initial loop and options logic

---
+++ README.md
@@ -0,0 +1,10 @@
+# auctionrs
+
+## Goals
+- Create a client-server system for an auction network.
+- The centralized server instance will be the entity resposnsible for keeping the data and managing bidding at auctions.
+- Users are allowed to post their products and bid in other user's auctions.
+- Ultimately, the system should correctly handle a big amount of users simultaneously bidding in products, with real-time updates of the values.
+
+## Development
+I will keep a record with the progress of the development of the tool. The idea is to start small, with a single user and no parallelism or APIs, so we can track progress and add features when necessary.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+struct User {
+    id: u8,
+    name: String,
+    products: Vec<Product>,
+}
+
+struct Product {
+    id: u8,
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user = User {
+        id: 0,
+        name: String::from("John Doe"),
+        products: Vec::new(),
+    };
+
+    println!("Welcome back {}!", user.name);
+
+    let mut online = true;
+
+    while online {
+        println!(
+            "What would you like to do today?\n[1] Post a product\n[2] List available auctions\n[3] Exit\n"
+        );
+
+        let mut input = String::new();
+
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        let cmd = input.trim().parse();
+
+        match cmd {
+            Ok(opt) => match opt {
+                1 => println!("TODO\n"),
+                2 => println!("TODO\n"),
+                3 => online = false,
+                _ => println!("Please enter a valid option!\n"),
+            },
+            Err(_) => println!("Please enter a valid option!\n")
+        }
+    }
+
+    println!("Exiting...");
 }
